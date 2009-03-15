e98efbaebd01ba41aa70db55926e260881634c47
Jamis Buck <jamis@37signals.com>
2009-03-14 21:45:14 -0600

    bucket perma, showing expenses against a specific bucket

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,15 +1,26 @@
 class BucketsController < ApplicationController
   before_filter :find_account, :only => :index
+  before_filter :find_bucket, :only => :show
 
   def index
   end
 
+  def show
+    @page = (params[:page] || 0).to_i
+    @more_pages, @items = bucket.line_items.page(@page)
+  end
+
   protected
 
-    attr_reader :account
-    helper_method :account
+    attr_reader :account, :bucket
+    helper_method :account, :bucket
 
     def find_account
       @account = subscription.accounts.find(params[:account_id])
+    end
+
+    def find_bucket
+      @bucket = Bucket.find(params[:id])
+      @account = subscription.accounts.find(@bucket.account_id)
     end
 end
--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,8 +1,20 @@
 class Bucket < ActiveRecord::Base
+  DEFAULT_PAGE_SIZE = 100
+
   belongs_to :account
   belongs_to :author, :class_name => "User", :foreign_key => "user_id"
 
-  has_many :line_items
+  has_many :line_items do
+    def page(n, options={})
+      size = options.fetch(:size, DEFAULT_PAGE_SIZE)
+      records = find(:all, :include => { :event => :line_items },
+        :order => "occurred_on DESC",
+        :limit => size + 1,
+        :offset => n * size)
+
+      [records.length > size, records[0,size]]
+    end
+  end
 
   def balance
     @balance ||= line_items.sum(:amount) || 0
