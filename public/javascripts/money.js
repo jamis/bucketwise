40d6bb023c83ce6b3284248f3e556fa423c14f0b
Jamis Buck <jamis@37signals.com>
2009-03-19 15:02:20 -0600

    Manually parse money values to avoid floating-point round-off errors

--- public/javascripts/money.js
+++ public/javascripts/money.js
@@ -3,18 +3,31 @@ var Money = {
     return (Math.abs(cents) / 100).toFixed(2);
   },
 
-  cents: function(dollars) {
-    return Math.floor(Math.abs(dollars) * 100);
+  parse: function(field, keepNegative) {
+    return Money.parseValue($F(field));
   },
 
-  parse: function(field, keepNegative) {
-    var value = $F(field).gsub(/[^-+\d.]/, "");
-    var raw = parseFloat(value);
-    var sign = 1;
+  /* don't want to use parseFloat, because we can be subject to
+   * floating point round-off */
+  parseValue: function(string, keepNegative) {
+    var value = string.gsub(/[^-+\d.]/, "");
+    var match = value.match(/^([-+]?)(\d*)(?:\.(\d+))?$/);
 
-    if(raw < 0 && keepNegative) sign = -1;
+    if(!match) return 0;
 
-    var result = Money.cents(raw);
-    return isNaN(result) ? 0 : (sign * result);
+    var sign = (match[1] == "-" && keepNegative) ? -1 : 1;
+    if(match[2].length > 0)
+      var dollars = parseInt(match[2]);
+    else
+      var dollars = 0;
+
+    if(match[3] && match[3].length > 0) {
+      var cents_magnitude = Math.pow(10, match[3].length);
+      var cents = Math.round(parseInt(match[3].sub(/^0+/, "")) * 100 / cents_magnitude);
+    } else {
+      var cents = 0;
+    }
+
+    return sign * (dollars * 100 + cents);
   }
 }
