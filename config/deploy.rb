5e48b3fa41f4af1ac70f58d0209cb959e606a166
Jamis Buck <jamis@37signals.com>
2009-03-22 23:36:05 -0600

    make default capfile link to local file for custom deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
 # FIXME: common deployment stuff here, with a conditional load of the real
 # recipe file, so that the real stuff can exist locally without being
 # checked into the main repo.
+
+load File.expand_path("~/.buckets/Capfile")
