6b37181bb25388825b5d6a2c1edbd24be77a511e
Jamis Buck <jamis@37signals.com>
2009-04-02 22:14:54 -0600

    make sure bucket assimilation updates bucket balance

--- test/unit/bucket_test.rb
+++ test/unit/bucket_test.rb
@@ -4,11 +4,14 @@ class BucketTest < ActiveSupport::TestCase
   test "assimilate should change all references of argument to self and destroy argument" do
     groceries = buckets(:john_checking_groceries)
     dining = buckets(:john_checking_dining)
+    old_groceries_balance = groceries.balance
+    old_dining_balance = dining.balance
 
     groceries.assimilate(dining)
 
     assert !Bucket.exists?(dining.id)
     assert_equal groceries, line_items(:john_lunch_checking_dining).bucket
+    assert_equal buckets(:john_checking_groceries, :reload).balance, old_groceries_balance + old_dining_balance
   end
 
   test "assimilate bucket from different account should raise exception and make no change" do
