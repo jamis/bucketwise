aaab55b298ab0de8bf4298644678b04a6e08b60c
Jamis Buck <jamis@37signals.com>
2009-04-17 19:13:39 -0600

    split the session info out so it can easily be overriden
    this lets me commit a usable configuration, and yet use
    a different one in my own bucketwise deployment

--- config/environment.rb
+++ config/environment.rb
@@ -13,13 +13,5 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.load_paths += %W( #{RAILS_ROOT}/app/concerns )
   config.time_zone = 'UTC'
-
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session = {
-    :session_key => '_bucketwise_session',
-    :secret      => '2f8e38ad3db3c9c3be9e60866b6d9137919c9ffb96431d6c0620217fe99fae08b1860b13a133dccd953192cc6b3dfb232031a00a6baa6cf7ce70edde39db5b09'
-  }
+  config.action_controller.session = YAML.load_file("#{RAILS_ROOT}/config/session.yml")
 end
