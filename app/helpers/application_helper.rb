5a13d4dfc1b0907ad1eb60b58d6c59ec76898b67
Jamis Buck <jamis@37signals.com>
2009-04-18 23:00:09 -0600

    indicate current version and last deployment on each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,21 @@ module ApplicationHelper
       "display: none"
     end
   end
+
+  def application_version
+    @application_version ||= if File.exists?("#{RAILS_ROOT}/REVISION")
+      File.read("#{RAILS_ROOT}/REVISION").strip
+    else
+      "(development)"
+    end
+  end
+
+  def application_last_deployed
+    @application_last_deployed ||= if File.exists?("#{RAILS_ROOT}/REVISION")
+      deployed_at = File.stat("#{RAILS_ROOT}/REVISION").ctime
+      time_ago_in_words(deployed_at)
+    else
+      "(not deployed)"
+    end
+  end
 end
