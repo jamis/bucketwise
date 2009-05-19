c6f6dd118a70cd7e3ec374ca4a564682f6b71a9b
Kieran Pilkington <kieran@katipo.co.nz>
2009-05-20 11:36:16 +1200

    bugfix: when upgrading and the limit was nil, errors would occur. The negative class also wasn't having much effect and the light grey number were hardly visible

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -14,7 +14,7 @@ module SubscriptionsHelper
     classes << "negative" if balance < 0
     classes << "current_balance"
 
-    if container.is_a?(Account) && container.credit_card?
+    if container.is_a?(Account) && container.credit_card? && !container.limit.blank?
       percentage_used = container.limit.abs.to_i == 0 ? 100 :
         ((container.balance.abs.to_f / container.limit.abs.to_f) * 100).to_i
       classes << if percentage_used >= Account::DEFAULT_LIMIT_VALUES[:critical]:  "critical"
