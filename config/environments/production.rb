88851a7fe64653ca05dc8030e6cb6f8570fbaf32
Artur Komarov <artur.komarov@gmail.com>
2021-01-12 17:19:55 +0700

    Resolve not precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.assets.check_precompiled_asset = false
 end
